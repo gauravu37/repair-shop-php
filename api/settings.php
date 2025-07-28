806f4f5c7a7eb4196aa09f7c3485930b8145efc3
Gaurav Mittal <er.gauravmittal1989@gmail.com>
2025-07-28 11:58:12 +0530

    setting correction

--- api/settings.php
+++ api/settings.php
@@ -3,11 +3,11 @@
 // settings.php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: GET, POST, PUT");
+header("Access-Control-Allow-Methods: POST, GET, PUT, DELETE");
+header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-require_once 'config/Database.php';
-//require_once 'models/Setting.php';
+require_once __DIR__ . '/../config/Database.php';
 require_once __DIR__ . '/../classes/Setting.php';
 
 $database = new Database();
